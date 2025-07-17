0074a4222ce77e7c2b6a733248a3ddb40b410ff7
Gunish Sharma <gunishsharma20@gmail.com>
2025-07-17 17:38:53 -0600

    automated git deletion

---
+++ day17/WeSplit2/WeSplit2/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeSplit2
+//
+//  Created by Gunish Sharma on 2025-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ day17/WeSplit2/WeSplit2/WeSplit2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit2App.swift
+//  WeSplit2
+//
+//  Created by Gunish Sharma on 2025-07-16.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

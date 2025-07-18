2f1a634d051e89d493a215139fb4faef29bb2a6b
Gunish Sharma <gunishsharma20@gmail.com>
2025-07-18 02:10:14 -0600

    day18 completed

---
+++ day18/WeSplit3/WeSplit3/WeSplit3App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit3App.swift
+//  WeSplit3
+//
+//  Created by Gunish Sharma on 2025-07-18.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

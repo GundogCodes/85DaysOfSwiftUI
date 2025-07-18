064be7fd99927528efa9f2f8ed4d9693efd4d6ab
Gunish Sharma <gunishsharma20@gmail.com>
2025-07-18 01:53:30 -0600

    day17 completed

--- day17/WeSplit2/WeSplit2/ContentView.swift
+++ day17/WeSplit2/WeSplit2/ContentView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    @State private var checkAmount = 0.0
+    @State private var numOfPeople = 0
+    @State private var tipPercentage = 0.2
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [0,0.1,0.15,0.2,0.25,0.5]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numOfPeople + 2 )
+        let tipSelection = Double(tipPercentage)
         
+        let tipValue = (checkAmount) * tipSelection
+        let grandTotal  = checkAmount + tipValue
+        
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form{
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numOfPeople) {
+                        ForEach(2..<100){
+                                Text("\($0) people")
+                            
+                        }
+                    }
+//                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("How much do you want to tip?") {
+//                    Text("How much do you want to tip?")
+                    Picker("Tip Percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id:\.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Total Per Person") {
+                    
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "CAD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                    
+                }
+            }
+        }
     }
 }
 

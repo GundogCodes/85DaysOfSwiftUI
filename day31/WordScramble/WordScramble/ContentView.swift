da3e4c6beb7b4b5eb03e8dc8e8efe8f6ca2f7b5c
Gunish Sharma <gunishsharma20@gmail.com>
2025-08-06 18:50:02 -0600

    day31 completed

---
+++ day31/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,140 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Gunish Sharma on 2025-08-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var  showingError = false
+    @State private var score = 0
+    var body: some View {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .textInputAutocapitalization(.never)
+                }
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName:"\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK") { }
+            } message: {
+                Text(errorMessage)
+            }
+            .toolbar{
+                Button("Restart") {
+                    startGame()
+                }
+            }
+            Text("Score \(score)")
+        }
+        
+        
+    }
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {return}
+        
+        guard answer.count > 0 else { return }
+        
+        guard tooShort(word: answer) else {
+            wordError(title: "Word is too short", message: "Word must be 8 letters!")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'")
+            return
+        }
+        
+        guard isRealWord(word: answer) else {
+            wordError(title: "Word not recognizes", message: "Your can't just make them up, you know!")
+            return
+        }
+        
+        score += 1
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL, encoding: .ascii) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+            return false
+            }
+        }
+        return true
+    }
+    
+    func tooShort(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
+        
+        return true
+    }
+    
+    func isRealWord(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ day31/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Gunish Sharma on 2025-08-06.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
